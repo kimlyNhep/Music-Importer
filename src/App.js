d41a46ec350e712372f1ed72a036c760dcae8d07
kimlynhep <kimly.nhep0168@gmail.com>
2020-07-01 00:11:04 +0700

    create login ui

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import LoginBox from './components/Authentication/login';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Back-img"></div>
+      <div className="Auth-box">
+        <LoginBox />
+      </div>
     </div>
   );
 }
